af861ffe2b2c79b99a7d00b1d8a1fac178d1940c
Luciano Ferreira <lfbnwork@gmail.com>
2022-08-23 09:32:09 +0200

    Delete client directory

--- client/src/models/repo.ts
+++
@@ -1,5 +0,0 @@
-//export const server = "http://localhost:5000"
-export const server = "https://w3link-api.vercel.app"
-export const create_account_path = "/create-account"
-export const set_data_path = "/set-data"
-export const get_data_path = "/get-data"
