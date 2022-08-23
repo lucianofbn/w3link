a6a602dba028396aee8766b56670e43e10ad27c3
Luciano Ferreira <lfbnwork@gmail.com>
2022-08-23 15:55:34 +0200

    fix dot routes

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,13 +1,8 @@
-//import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import pluginRewriteAll from "vite-plugin-rewrite-all";
-
-// https://vitejs.dev/config/
-export default ({
-  plugins: [react(), pluginRewriteAll()],
-});
+//import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 
-/*export default defineConfig({
+export default defineConfig({
   plugins: [react()]
-}) */
+})
